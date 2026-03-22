6c47d29fca5d14fbb690a0d179135e3968feed58
Filipe-Kiyoshi-Otuka <Filipeotuka@usp.br>
2026-03-22 13:33:58 -0300

    Adicionei o idh mínimo e máximo ao idh médio. e também criei uma tabela para comparar o idh por município visualmente, com os municípios no eixo X os municípios, o idh no eixo Y

--- analise.R
+++ analise.R
@@ -2,7 +2,18 @@
 
 library("tidyverse")
 library("readr")
+library("ggplot2")
 
 df <- read.csv("C:/Users/filip/OneDrive/Documentos/metodos/municipios_virgula.csv")
 idh <- df %>%
-  summarise(idh_medio = mean(idh))
+  summarise(idh_minimo = min(idh),
+            idh_maximo = max(idh),
+            idh_medio = mean(idh))
+
+df <- read.csv("C:/Users/filip/OneDrive/Documentos/metodos/municipios_virgula.csv")
+ggplot(df, aes(x = nome_municipio, y = idh))+
+  geom_col(fill = "red")+
+  labs(tittle = "Idh por município",
+       x = "municípios",
+       y = "IDH")
+df
